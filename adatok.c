3b5564ba6c75790313eb7a4acd9905d2c879c4fe
rmeszes <rmeszes1@gmail.com>
2022-11-26 21:21:51 +0100

    működő keresés wildcarddal

--- adatok.c
+++ adatok.c
@@ -275,3 +275,167 @@ void nevjegy_modositasa(Nevjegyek *lista, char *fajlnev) {
         fajlba_ment(lista, fajlnev);
     }
 }
+
+bool strmatch(const char str[], const char pattern[])
+{
+    int n = strlen(str);
+    int m = strlen(pattern);
+
+    if (m == 0)
+        return (n == 0);
+
+    bool lookup[n + 1][m + 1];
+
+
+    memset(lookup, false, sizeof(lookup));
+
+
+    lookup[0][0] = true;
+
+
+    for (int j = 1; j <= m; j++)
+        if (pattern[j - 1] == '*')
+            lookup[0][j] = lookup[0][j - 1];
+
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            if (pattern[j - 1] == '*')
+                lookup[i][j]
+                        = lookup[i][j - 1] || lookup[i - 1][j];
+            else if (pattern[j - 1] == '?'
+                     || str[i - 1] == pattern[j - 1])
+                lookup[i][j] = lookup[i - 1][j - 1];
+
+                // If characters don't match
+            else
+                lookup[i][j] = false;
+        }
+    }
+
+    return lookup[n][m];
+}
+
+static int kereses_darab(Nevjegyek* lista, int mezo, char *feltetel) {
+    int darab = 1;
+    Nevjegy *mozgo = lista->elso->kov;
+    if (mezo == 1) {
+        printf("Keresett név:");
+        char input[301];
+        gets(input);
+        strcpy(feltetel, input);
+
+        while (mozgo->kov != NULL) {
+            if (strmatch(mozgo->nev, feltetel)) darab++;
+            mozgo = mozgo->kov;
+        }
+    }
+    else if (mezo == 2) {
+        printf("Keresett telefonszám:");
+        char input[20];
+        gets(input);
+        strcpy(feltetel, input);
+
+        while (mozgo->kov != NULL) {
+            if (strmatch(mozgo->telefonszam, feltetel)) darab++;
+            mozgo = mozgo->kov;
+        }
+    }
+    else {
+        printf("Keresett email:");
+        char input[321];
+        gets(input);
+        strcpy(feltetel, input);
+
+        while (mozgo->kov != NULL) {
+            if (strmatch(mozgo->email, feltetel)) darab++;
+            mozgo = mozgo->kov;
+        }
+    }
+    return darab;
+}
+
+
+static void kereso(Nevjegyek *source, int mezo, char *feltetel, Nevjegy** cel) {
+    Nevjegy* mozgo = source->elso->kov;
+    int cel_id = 1;
+    if (mezo == 1) {
+        while (mozgo->kov != NULL) {
+            if (strmatch(mozgo->nev, feltetel)) {
+                cel[cel_id++] = mozgo;
+            }
+            mozgo = mozgo->kov;
+        }
+    } else if (mezo == 2) {
+        while (mozgo->kov != NULL) {
+            if (strmatch(mozgo->telefonszam, feltetel)) {
+                cel[cel_id++] = mozgo;
+            }
+            mozgo = mozgo->kov;
+        }
+    } else {
+        while (mozgo->kov != NULL) {
+            if (strmatch(mozgo->email, feltetel)) {
+                cel[cel_id++] = mozgo;
+            }
+            mozgo = mozgo->kov;
+        }
+    }
+}
+
+static void kereses_almenu(Nevjegy* elem) {
+    system("cls");
+    printf("NÉVJEGY\n"
+           "\tNév: %s\n"
+           "\tTelefonszám: %s\n"
+           "\tEmail cím: %s\n"
+           "Visszalépés # karakterrel\n", elem->nev, elem->telefonszam, elem->email);
+
+    char input;
+
+    scanf(" %c", &input);
+    while (input != '#') {
+        printf("Érvénytelen bemenet!\n");
+        //while(getchar() != '\n');
+        scanf(" %c", &input);
+    }
+}
+
+void kereses(Nevjegyek * lista) {
+    system("cls");
+    printf("Mi alapján szeretnél keresni?\n"
+           "\t1. Név\n"
+           "\t2. Telefonszám\n"
+           "\t3. Email cím\n");
+    int mezo;
+    scanf("%d", &mezo);
+    while (getchar() != '\n') {
+        if (getchar() == '#') return;
+    }
+    while (mezo <= 1 & mezo >= 3) {
+        printf("Érvénytelen menüpont!");
+        scanf("%d", &mezo);
+        while (getchar() != '\n') {
+            if (getchar() == '#') return;
+        }
+    }
+    system("cls");
+    char feltetel[321];
+    int db = kereses_darab(lista, mezo, feltetel);
+    Nevjegy* talalatok[db];
+    kereso(lista, mezo, feltetel, talalatok);
+    system("cls");
+    printf("KERESÉSI TALÁLATOK:\n");
+    for (int i = 1; i < db; ++i) {
+        printf("\t%d. %s\n", i, talalatok[i]->nev);
+    }
+    int valasztott;
+    if (scanf("%d", &valasztott) == 1) {
+        if (valasztott > 0 & valasztott <= db) {
+            kereses_almenu(talalatok[valasztott]);
+        }
+    } else {
+        while(getchar() != '\n');
+    }
+}
